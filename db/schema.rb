a74b74e9b69b559a6497c28b7911bb976de9b1d6
Dan Kleiman <info@dankleiman.com>
2014-06-10 10:23:58 -0400

    removed favorites columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610140847) do
+ActiveRecord::Schema.define(version: 20140610142048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: true do |t|
-    t.string  "title",    null: false
+    t.string  "title",  null: false
     t.string  "author"
-    t.boolean "favorite"
     t.integer "rating"
   end
 
