188284629b3eddca6af815964a139ba3a243c75a
Dan Kleiman <info@dankleiman.com>
2014-06-10 10:30:41 -0400

    create checkouts table

---
+++ db/migrate/20140610142802_create_checkouts_table.rb
@@ -0,0 +1,9 @@
+class CreateCheckoutsTable < ActiveRecord::Migration
+  def change
+    create_table :checkouts do |table|
+      table.integer :book_id, null: false
+
+      table.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610142048) do
+ActiveRecord::Schema.define(version: 20140610142802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20140610142048) do
     t.string  "title",  null: false
     t.string  "author"
     t.integer "rating"
+  end
+
+  create_table "checkouts", force: true do |t|
+    t.integer  "book_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
