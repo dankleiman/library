8cc2e73e4996f56a8bcdc1a2b1b54edefda79d9c
Dan Kleiman <info@dankleiman.com>
2014-06-10 10:10:32 -0400

    added rating column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610140141) do
+ActiveRecord::Schema.define(version: 20140610140847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140610140141) do
     t.string  "title",    null: false
     t.string  "author"
     t.boolean "favorite"
+    t.integer "rating"
   end
 
 end
